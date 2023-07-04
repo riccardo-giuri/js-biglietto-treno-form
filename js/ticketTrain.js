2b9418c77c2b7b41e82044a838c394dc3f6a5f30
riccardo-giuri <richicryst@gmail.com>
2023-07-04 11:40:27 +0200

    bonuses done

--- js/ticketTrain.js
+++ js/ticketTrain.js
@@ -2,25 +2,26 @@
 
 let username;
 let kilometers;
-let age;
+let age = document.querySelector(".form-select");
 const kmPrice = 0.21;
 let underAgeCouponPrecentage = 20;
 let overAgeCouponPrecentage = 40;
 let couponValue = 0;
 let finalPrice = 0;
-const Login_btn = document.querySelector(".generate_btn");
+const loginBtn = document.querySelector(".generate_btn");
+const cancelBtn = document.querySelector(".cancel_btn");
 
 
-Login_btn.addEventListener("click", function () {
+loginBtn.addEventListener("click", function () {
     const username = document.querySelector("[name='username']").value;
     const kilometers = document.querySelector("[name='kilometers']").value;
-    const age = document.querySelector("[name='age']").value;
+    const ageValue = age.options[age.selectedIndex].text;
     const ridePrice = (kilometers * kmPrice);
 
     console.log(username, kilometers, age);
 
-    switch (true){
-        case age < 18:
+    switch (ageValue){
+        case "Minorenne":
             couponValue = (ridePrice / 100) * underAgeCouponPrecentage;
             finalPrice = (ridePrice - couponValue).toFixed(2);
             document.querySelector(".t_name").innerHTML = username;
@@ -30,7 +31,7 @@ Login_btn.addEventListener("click", function () {
             document.querySelector(".t_price").innerHTML = `${finalPrice}€`;
             break;
     
-        case age > 65:
+        case "Anziano":
             couponValue = (ridePrice / 100) * overAgeCouponPrecentage;
             finalPrice = (ridePrice - couponValue).toFixed(2);
             document.querySelector(".t_name").innerHTML = username;
@@ -50,5 +51,13 @@ Login_btn.addEventListener("click", function () {
     }
 })
 
-
+cancelBtn.addEventListener("click", function () {
+    document.querySelector("[name='username']").value = "";
+    document.querySelector("[name='kilometers']").value = "";
+    document.querySelector(".t_name").innerHTML = "--";
+    document.querySelector(".t_offer").innerHTML = "--";
+    document.querySelector(".t_carriage").innerHTML = "--";
+    document.querySelector(".t_codeCP").innerHTML = "--";
+    document.querySelector(".t_price").innerHTML = "--";
+})
 
